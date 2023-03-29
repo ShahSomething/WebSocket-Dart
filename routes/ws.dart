ceef9cd05577ea9a2d753c5ed435fc39b3cde0a8
Shah Raza <shahsomething@yahoo.com>
2023-03-29 05:44:04 +0500

    WebSocket handler added

--- routes/ws.dart
+++ routes/ws.dart
@@ -1,5 +1,22 @@
-import 'package:dart_frog/dart_frog.dart';
+// ignore_for_file: avoid_print
 
-Response onRequest(RequestContext context) {
-  return Response(body: 'You have requested /ws');
+import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  final handler = webSocketHandler((channel, protocol) {
+    // A new client has connected to our server.
+    print('connected');
+
+    // Send a message to the client.
+    channel.sink.add('hello from the server');
+
+    // Listen for messages from the client.
+    channel.stream.listen(
+      print,
+      // The client has disconnected.
+      onDone: () => print('disconnected'),
+    );
+  });
+  return handler(context);
 }
